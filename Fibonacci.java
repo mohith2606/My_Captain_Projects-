22493e59a2f84b5a91d59105c4aa712f0ead837f
Mohith <73785108+RstarGR@users.noreply.github.com>
2022-04-07 14:06:58 +0530

    Update Fibonacci.java

--- Fibonacci.java
+++ Fibonacci.java
@@ -1,14 +1,5 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package my_captain_java;
 
-/**
- *
- * @author MOHITH
- */
 public class Fibonacci {
     public static void main(String[]args){
         int n1=0,n2=1,n3,i,count=11;
